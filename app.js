0edc5cadf99d980e43788b8e4b4548b6f4f37b39
Anisha Lopes <155230123+AnishaLopes@users.noreply.github.com>
2024-11-26 06:19:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById(navbar_menu);
+
+function history(){
+    alert("You are displayed with History of Nanotechnology");
+}
+
+function char(){
+    alert("The characteristics of Nanotechnology")
+}
